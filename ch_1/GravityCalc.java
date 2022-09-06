07927a5940d4d681ad3a49a8400db23bebd9273f
Delta.v <tmm.michalski@gmail.com>
2022-09-06 15:27:07 +0200

    Add chapter 1 examples

---
+++ ch_1/GravityCalc.java
@@ -0,0 +1,9 @@
+public class GravityCalc {
+    public static void main(String[] args) {
+        float myWeight;
+        myWeight = 70;
+        System.out.println("My weight on Earth is: " + myWeight);
+        System.out.println("My weight on the Moon is: " + myWeight * 0.17);
+
+    }
+}
